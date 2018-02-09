8d0cf4e8699ea835b0ded66dcd6ce25461246491
programming-john <31251360+programming-john@users.noreply.github.com>
2018-02-09 09:43:43 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -14,14 +14,14 @@ app = Flask(__name__)
 
 app.debug = True #Change this to False for production
 
-app.secret_key = os.environ['SECRET_KEY'] 
+app.secret_key = os.environ['SECRET_KEY'] #use SECRET_KEY to sign session cookies
 oauth = OAuth(app)
 
-
+#Set up GitHub as OAuth provider
 github = oauth.remote_app(
     'github',
-    consumer_key=os.environ['GITHUB_CLIENT_ID'], 
-    consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],
+    consumer_key=os.environ['GITHUB_CLIENT_ID'], #your web app's "username" for github OAuth
+    consumer_secret=os.environ['GITHUB_CLIENT_SECRET'], #your webapp's "password" for github OAuth
     request_token_params={'scope': 'user:email'}, #request read-only access to the user's email.  For a list of possible scopes, see developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps
     base_url='https://api.github.com/',
     request_token_url=None,
@@ -39,8 +39,9 @@ def inject_logged_in():
 def home():
     return render_template('home.html')
 
+#redirect to GitHub's OAuth page and confirm the callback URL
 @app.route('/login')
-def login():   
+def login():
     return github.authorize(callback=url_for('authorized', _external=True, _scheme='https'))
 
 @app.route('/logout')
@@ -48,7 +49,7 @@ def logout():
     session.clear()
     return render_template('message.html', message='You were logged out')
 
-@app.route()#the route should match the callback URL registered with the OAuth provider
+@app.route('login/authorized')#the route should match the callback URL registered with the OAuth provider
 def authorized():
     resp = github.authorized_response()
     if resp is None:
@@ -67,13 +68,21 @@ def renderPage1():
     if 'user_data' in session:
         user_data_pprint = pprint.pformat(session['user_data'])#format the user data nicely
     else:
-        user_data_pprint = '';
-    return render_template('page1.html',dump_user_data=user_data_pprint)
+        try:
+            #save user data and set log in message
+            session['github_token']=(resp['access_token'], '')
+            session['user_data']=github.get('user').data
+            message= "You were successfully logged in as " + session['user_data']['login']
+        except:
+            session.clear()
+            message='Unable to login. Please try again.'
+            #clear the session and give error message
+    return render_template('message.html', message=message)
 
 @app.route('/page2')
 def renderPage2():
     return render_template('page2.html')
-
+#the tokengetter is automatically called to chck who is logged in
 @github.tokengetter
 def get_github_oauth_token():
     return session.get('github_token')
